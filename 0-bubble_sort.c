d830cdd289890cc65bafeb9327b35217cc9504c7
TeeMeeLehin <samemerald8@gmail.com>
2023-07-22 15:17:52 +0100

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+* bubble_sort - function to implement nubble sort algorithm
+* @array: the list to be sorted
+* @size: size of the list
+* Return: void
+*/
+void bubble_sort(int *array, size_t size)
+{
+size_t i, j;
+int temp;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array((const int *)array, size);
+}
+}
+}
+}
