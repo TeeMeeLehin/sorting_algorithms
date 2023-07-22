bbe16d45e25e332e3c56e87c9628f5d0b6834d85
TeeMeeLehin <samemerald8@gmail.com>
2023-07-22 15:31:31 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,7 +9,7 @@
 void bubble_sort(int *array, size_t size)
 {
 size_t i, j;
-int temp;
+int temp, flag = 0;
 
 for (i = 0; i < size; i++)
 {
@@ -21,7 +21,13 @@ temp = array[j];
 array[j] = array[j + 1];
 array[j + 1] = temp;
 print_array((const int *)array, size);
+flag++;
 }
+}
+if (flag == 0)
+{
+print_array((const int *)array, size);
+return;
 }
 }
 }
