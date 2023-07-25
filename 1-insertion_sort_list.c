731a3de3a29e8aa343d524f6a30addf5d42e1ad9
TeeMeeLehin <samemerald8@gmail.com>
2023-07-25 22:47:18 +0100

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 listint_t *sorted, *current, *temp;
 if (list == NULL || *list == NULL || (*list)->next == NULL)
 return;
-sorted = NULL; 
+sorted = NULL;
 
 while (*list)
 {
@@ -46,5 +46,5 @@ print_list(current);
 }
 }
 
-*list = sorted; 
+*list = sorted;
 }
