663d31d5bbdbcf7a14355c6f69af3025fff3c545
TeeMeeLehin <samemerald8@gmail.com>
2023-07-25 15:42:07 +0100

    sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,22 +8,26 @@
 */
 void selection_sort(int *array, size_t size)
 {
-size_t j, i, min, n = 0;
+size_t i, j, min;
 int temp;
-for (j = 0; j < size; j++)
-{
-min = n;
-for (i = n; i < size; i++)
-{
-if (array[i] < array[min])
+for (i = 0; i < size - 1; i++)
 {
 min = i;
+
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min])
+{
+min = j;
 }
 }
-temp = array[n];
-array[n] = array[min];
+if (min != i)
+{
+temp = array[i];
+array[i] = array[min];
 array[min] = temp;
+}
 print_array((const int *)array, size);
-n++;
 }
 }
+
